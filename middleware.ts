16a6e2441585d37dbd7b6d2cf0eaa85c0b2258af
山内健裕 <>
2024-12-06 17:43:33 +0900

    no message

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(request: NextRequest) {
-  console.log("middleware:" + request.url);
-  return NextResponse.next();
-}
-
-export const config = {};
